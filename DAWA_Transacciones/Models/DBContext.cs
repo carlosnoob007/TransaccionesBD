32e62cfa4579f2652854129ce22384cfcf1124ca
carlosnoob007 <carlosfcj16@gmail.com>
2023-07-04 22:37:00 -0500

    first commit

---
+++ DAWA_Transacciones/Models/DBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DAWA_Transacciones.Models
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<Cantante> Cantante{ get; set; }
+        public DbSet<Album> Album { get; set; }
+        public DbSet<Musica> Musica { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-AI06HEJ8;Database=MusicaDB;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
---
+++ DAWA_Transacciones/Program.cs
@@ -0,0 +1,55 @@
+﻿using DAWA_Transacciones.Models;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        addTemaSolistaTransaction();
+    }
+
+    public static void addTemaSolistaTransaction()
+    {
+        Console.WriteLine("Metodo agregar tema musical de solista");
+
+        DBContext context = new DBContext();
+        Cantante cantante = new Cantante();
+        Album album = new Album();
+        Musica musica = new Musica();
+        var transaction = context.Database.BeginTransaction();
+
+        try
+        {
+            //Agregar Autor
+            cantante.Nombre = "Daddy Yankee";
+            cantante.Nacionalidad = "Puertoriqueño";
+            cantante.Genero = 'M';
+            cantante.Estado = true;
+            context.Cantante.Add(cantante);
+            context.SaveChanges();
+
+            //Agregar Album
+            album.Nombre = "Barrio Fino";
+            album.Periodo = 2004;
+            album.Sello = "Cartel Record";
+            album.Estado = true;
+            context.Album.Add(album);
+            context.SaveChanges();
+
+            //Agregar Cancion
+            musica.Nombre = "Gasolina";
+            musica.AutorId = cantante.AutorId;
+            musica.AlbumId = album.AlbumId;
+            musica.Estado = true;
+            context.Musica.Add( musica);
+            context.SaveChanges();
+
+            transaction.Commit();
+            Console.WriteLine("Datos agregados exitosamente");
+        }
+        catch (Exception e)
+        {
+            transaction.Rollback();
+            Console.WriteLine("Error " + e.ToString());
+        }
+    }
+}
